b333b218c5c374ed39b7e6ccd6db498c2454fa60
Dario Antonini <dario.antonini@heig-vd.ch>
2022-12-19 13:18:52 +0100

    modify start page

--- src/app/shared/widgets/widgets.module.ts
+++ src/app/shared/widgets/widgets.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormInputTextComponent } from './form-input-text/form-input-text.component';
-
+import { ButtonLoginComponent } from './button-login/button-login.component';
 
 
 @NgModule({
   declarations: [
-    FormInputTextComponent
+    FormInputTextComponent,
+    ButtonLoginComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    FormInputTextComponent
+    FormInputTextComponent,
+    ButtonLoginComponent,
   ]
 })
 export class WidgetsModule { }
