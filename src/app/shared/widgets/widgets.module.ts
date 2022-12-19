c827cbed5a0bfe523887ff73a70cae7a3d9a2fca
Witizli <benaiah.finkel@heig-vd.ch>
2022-12-19 12:01:51 +0100

    homePage Css Cercle vert degrade

--- src/app/shared/widgets/widgets.module.ts
+++ src/app/shared/widgets/widgets.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormInputTextComponent } from './form-input-text/form-input-text.component';
+import { CardProductComponent } from './card-product/card-product.component';
 
 
 
 @NgModule({
   declarations: [
-    FormInputTextComponent
+    FormInputTextComponent,
+    CardProductComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    FormInputTextComponent
+    FormInputTextComponent,
+    CardProductComponent
   ]
 })
 export class WidgetsModule { }
