73605fe84fc2daba412f5d3b3a97ddc6260bcb5f
Vincent <vincent.tarrit29@gmail.com>
2022-12-19 14:19:47 +0100

    setup up map

--- src/app/pages/explore/productor-map/productor-map.page.ts
+++ src/app/pages/explore/productor-map/productor-map.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-productor-map',
   templateUrl: './productor-map.page.html',
   styleUrls: ['./productor-map.page.scss'],
 })
-export class ProductorMapPage implements OnInit {
+export class ProductorMapPage implements AfterViewInit {
+  map: L.Map | undefined;
 
-  constructor() { }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [46.77529, 6.637531],
+      zoom: 16,
+    });
 
-  ngOnInit() {
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.map);
   }
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
