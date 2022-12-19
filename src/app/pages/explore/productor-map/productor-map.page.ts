52d3a65457f7decef0eef1750ba795d856d6b748
Vincent <vincent.tarrit29@gmail.com>
2022-12-19 15:52:48 +0100

    fix user null

--- src/app/pages/explore/productor-map/productor-map.page.ts
+++ src/app/pages/explore/productor-map/productor-map.page.ts
@@ -9,21 +9,27 @@ import * as L from 'leaflet';
 export class ProductorMapPage implements AfterViewInit {
   map: L.Map | undefined;
 
+  swissBounds = L.latLngBounds(L.latLng(45, 5), L.latLng(48, 11));
+
   private initMap(): void {
     this.map = L.map('map', {
       center: [46.77529, 6.637531],
-      zoom: 16,
+      zoom: 13,
+      maxBoundsViscosity: 1,
     });
 
     const tiles = L.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      'https://tile.osm.ch/switzerland/{z}/{x}/{y}.png',
       {
         maxZoom: 18,
-        minZoom: 3,
+        minZoom: 9,
+        bounds: this.swissBounds,
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }
     );
+
+    this.map.setMaxBounds(this.swissBounds);
 
     tiles.addTo(this.map);
   }
