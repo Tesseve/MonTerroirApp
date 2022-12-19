1515af8fe2ca081c1d0a0fd1f6665ad7304289fc
Witizli <benaiah.finkel@heig-vd.ch>
2022-12-19 14:15:18 +0100

    input Date Name generator

--- src/app/shared/widgets/widgets.module.ts
+++ src/app/shared/widgets/widgets.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormInputTextComponent } from './form-input-text/form-input-text.component';
 import { CardProductComponent } from './card-product/card-product.component';
+import { LocalPositionBarComponent } from './local-position-bar/local-position-bar.component';
 
 
 
 @NgModule({
   declarations: [
     FormInputTextComponent,
-    CardProductComponent
+    CardProductComponent,
+    LocalPositionBarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     FormInputTextComponent,
-    CardProductComponent
+    CardProductComponent,
+    LocalPositionBarComponent
   ]
 })
 export class WidgetsModule { }
