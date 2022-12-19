824bb353c5d262cab0b208ff8e58dd7922331df4
Vincent <vincent.tarrit29@gmail.com>
2022-12-19 14:00:28 +0100

    create app bar widget and map

--- src/app/shared/widgets/widgets.module.ts
+++ src/app/shared/widgets/widgets.module.ts
@@ -2,20 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormInputTextComponent } from './form-input-text/form-input-text.component';
 import { CardProductComponent } from './card-product/card-product.component';
-
-
+import { AppBarComponent } from './app-bar/app-bar.component';
 
 @NgModule({
-  declarations: [
-    FormInputTextComponent,
-    CardProductComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    FormInputTextComponent,
-    CardProductComponent
-  ]
+  declarations: [FormInputTextComponent, CardProductComponent, AppBarComponent],
+  imports: [CommonModule],
+  exports: [FormInputTextComponent, CardProductComponent, AppBarComponent],
 })
-export class WidgetsModule { }
+export class WidgetsModule {}
