91dbf526c1cfeb1f16f4d008bb1aa0090656cafa
Vincent <vincent.tarrit29@gmail.com>
2023-01-16 09:53:48 +0100

    clean code pipe

--- src/app/pipes/conversation-name.pipe.ts
+++ src/app/pipes/conversation-name.pipe.ts
@@ -10,15 +10,9 @@ export class ConversationNamePipe implements PipeTransform {
   auth = inject(AuthService);
 
   transform(conversation: Conversation, ...args: unknown[]): string {
-    const user = this.auth.getUser();
-    const userID = user?._id;
-
-    console.log(conversation);
-
-    //get the other user
-    const otherUser = conversation.users.find((user) => user._id !== userID);
-    console.log({ otherUser });
-
+    const otherUser = conversation.users.find(
+      (user) => user._id !== this.auth.getUser()?._id
+    );
     return otherUser?.username || 'Unknown';
   }
 }
