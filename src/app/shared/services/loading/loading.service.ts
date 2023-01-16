b884587b92de8046f14c9c173365e46f2e530e43
Vincent <vincent.tarrit29@gmail.com>
2023-01-16 10:58:19 +0100

    can force hiding loading

--- src/app/shared/services/loading/loading.service.ts
+++ src/app/shared/services/loading/loading.service.ts
@@ -7,10 +7,28 @@ import { LoadingController } from '@ionic/angular';
 export class LoadingService {
   constructor(private loadingController: LoadingController) {}
 
+  #canDisplayLoading = true;
+
+  forceHidingLoader() {
+    this.#canDisplayLoading = false;
+  }
+
+  forceDisplayingLoader() {
+    this.#canDisplayLoading = true;
+  }
+
+  async executeWithoutLoading<T>(callback: () => Promise<T>): Promise<T> {
+    this.forceHidingLoader();
+    const result = await callback();
+    this.forceDisplayingLoader();
+    return result;
+  }
+
   async showLoading(
     message?: string,
     { duration = 1000, backDrop = true } = {}
   ) {
+    if (!this.#canDisplayLoading) return;
     const loading = await this.loadingController.create({
       message: message || 'Chargement...',
       duration: duration,
@@ -18,10 +36,13 @@ export class LoadingService {
       backdropDismiss: backDrop ?? true,
     });
 
-    loading.present();
+    await loading.present();
   }
 
   async hideLoading() {
+    // check if there is any loading
+    const loading = await this.loadingController.getTop();
+    if (!loading) return;
     await this.loadingController.dismiss();
   }
 }
